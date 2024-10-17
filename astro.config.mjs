815b051a6795e76a5af75ed972e52847706f863e
suzza <suzza@susansilver.net>
2024-10-17 10:39:08 -0700

    add components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://hootgrowl.xyz",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
